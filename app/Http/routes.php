7d1df3110ababd37360d86ff95aeec432f33b457
Salman <jafrisalmanali@gmail.com>
2017-11-01 16:37:58 +0500

    Admin Page has been setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,5 +19,12 @@ Route::auth();
 
 Route::get('/home', 'HomeController@index');
 
+Route::get('/admin',    function()
+{
+
+    return view('admin.index');
+
+});
+
 
 Route::resource('admin/users','AdminUserController');
